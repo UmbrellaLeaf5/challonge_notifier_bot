4c73e953d7b7b761db16575c7170156920a303a9
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2025-06-05 21:42:00 +0300

    configure project with uv and ruff

---
+++ README.md
@@ -0,0 +1,114 @@
+# Challonge Notifier Bot
+
+## Installation
+
+0.  **Clone the repository:**
+
+    Before you begin, you need to clone the project's source code repository.
+ 
+    ```bash
+    git clone https://github.com/UmbrellaLeaf5/challonge_notifier_bot.git
+    ```
+
+    Go to the directory where the repository was cloned:
+
+    ```bash
+    cd challonge_notifier_bot
+    ``` 
+
+
+1.  **Creating a virtual environment:**
+
+    Open a terminal or command prompt in the root directory of your project (where the `requirements.txt` file is located) and run the following command to create a virtual environment named `.venv`:
+
+    ```bash
+    python3 -m venv .venv
+    ```
+
+    or
+
+    ```bash
+    python -m venv .venv
+    ```
+
+    *   If you only have `Python 3` installed, you can use `python` instead of `python3`.
+    *   If the virtual environment already exists (you created it earlier), skip this step.
+
+
+2.  **Activating the virtual environment:**
+
+    Activate the virtual environment so that `Python` uses the libraries installed inside it:
+
+    *   **Linux/macOS:**
+
+        ```bash
+        source .venv/bin/activate
+        ```
+
+    *   **Windows (Command Prompt):**
+
+        ```cmd
+        .venv\Scripts\activate
+        ```
+
+    *   **Windows (PowerShell):**
+
+        ```powershell
+        .venv\Scripts\Activate.ps1
+        ```
+
+    Once activated, you will see `(.venv)` at the beginning of the terminal line, indicating that the virtual environment is active.
+
+
+3.  **Installing dependencies from `requirements.txt`:**
+
+    Install all libraries listed in the `requirements.txt` file by running the following command:
+
+    ```bash
+    ./.venv/Scripts/pip install -r requirements.txt
+    ```
+
+    or simply:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+**Or just use [`uv`](https://github.com/astral-sh/uv):**
+
+```bash
+uv venv .venv
+```
+
+*   **Linux/macOS:**
+
+    ```bash
+    source .venv/bin/activate
+    ```
+
+*   **Windows (Git Bash):**
+
+    ```bash
+    source .venv/Scripts/activate
+    ```
+
+*   **Windows (Command Prompt):**
+
+    ```cmd
+    .venv\Scripts\activate
+    ```
+
+*   **Windows (PowerShell):**
+
+    ```powershell
+    .venv\Scripts\Activate.ps1
+    ```
+
+```bash
+uv pip install -r requirements.txt
+```
+
+P.S. one string:
+```bash
+uv venv .venv && source .venv/Scripts/activate && uv pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import requests
 import time
 
 # Conf
-TOKEN = '<TELEGRAM_BOT_TOKEN>'
-CHALLONGE_API_KEY = '<CHALLONGE_API_KEY>'
-TOURNAMENT_URL = '<TOURNAMENT_ID>'  # ID of the tournament, for example 'mytournament' in 'https://challonge.com/mytournament'
-CHAT_ID = '<CHAT_ID>' # Chat to send messages to
-COOKIES = {
-    '<place cookies here for connection>'
-}
+TOKEN = "<TELEGRAM_BOT_TOKEN>"
+CHALLONGE_API_KEY = "<CHALLONGE_API_KEY>"
+TOURNAMENT_URL = "<TOURNAMENT_ID>"  # ID of the tournament, for example 'mytournament' in 'https://challonge.com/mytournament'
+CHAT_ID = "<CHAT_ID>"  # Chat to send messages to
+COOKIES = {"<place cookies here for connection>"}
 
 
 bot = telebot.TeleBot(TOKEN)
@@ -18,69 +16,68 @@ bot = telebot.TeleBot(TOKEN)
 notified_matches = set()
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["start"])
 def start(message):
-    bot.reply_to(message, 'Bot is running!')
+  bot.reply_to(message, "Bot is running!")
 
 
 def fetch_matches():
-    """ get all matches from the tournament """
-    url = f'https://api.challonge.com/v1/tournaments/{TOURNAMENT_URL}/matches.json'
-    params = {'api_key': CHALLONGE_API_KEY}
-    headers = COOKIES
-    response = requests.get(url, 
-                            params=params,
-                            headers=headers)
-    response.raise_for_status()
-    return response.json()
+  """get all matches from the tournament"""
+  url = f"https://api.challonge.com/v1/tournaments/{TOURNAMENT_URL}/matches.json"
+  params = {"api_key": CHALLONGE_API_KEY}
+  headers = COOKIES
+  response = requests.get(url, params=params, headers=headers)
+  response.raise_for_status()
+  return response.json()
 
 
 def notify_matches():
-    """ check the match status and send notifications """
-    matches = fetch_matches()
-    for match in matches:
-        match_id = match['match']['id']
-        player1_id = match['match']['player1_id']
-        player2_id = match['match']['player2_id']
-        state = match['match']['state']
-        is_underway = match['match']['underway_at']
+  """check the match status and send notifications"""
+  matches = fetch_matches()
+  for match in matches:
+    match_id = match["match"]["id"]
+    player1_id = match["match"]["player1_id"]
+    player2_id = match["match"]["player2_id"]
+    state = match["match"]["state"]
+    is_underway = match["match"]["underway_at"]
 
-        if state == 'complete' and match_id in notified_matches:
-            player1 = fetch_participant_name(player1_id)
-            player2 = fetch_participant_name(player2_id)
-            winner = fetch_participant_name(match['match']['winner_id'])
-            message = f'🎉 <b>{winner}</b> — триумфатор матча\n{player1} <i>vs</i> {player2}! 🍻'
-            bot.send_message(CHAT_ID, message, parse_mode="HTML")
-            notified_matches.remove(match_id)
+    if state == "complete" and match_id in notified_matches:
+      player1 = fetch_participant_name(player1_id)
+      player2 = fetch_participant_name(player2_id)
+      winner = fetch_participant_name(match["match"]["winner_id"])
+      message = (
+        f"🎉 <b>{winner}</b> — триумфатор матча\n{player1} <i>vs</i> {player2}! 🍻"
+      )
+      bot.send_message(CHAT_ID, message, parse_mode="HTML")
+      notified_matches.remove(match_id)
 
-        if state == 'open' and is_underway and match_id not in notified_matches:
-            player1 = fetch_participant_name(player1_id)
-            player2 = fetch_participant_name(player2_id)
-            message = f'⚡️ Начинается матч:\n<b>{player1}</b> – <b>{player2}</b> 🍺'
-            bot.send_message(CHAT_ID, message, parse_mode="HTML")
-            notified_matches.add(match_id)
+    if state == "open" and is_underway and match_id not in notified_matches:
+      player1 = fetch_participant_name(player1_id)
+      player2 = fetch_participant_name(player2_id)
+      message = f"⚡️ Начинается матч:\n<b>{player1}</b> – <b>{player2}</b> 🍺"
+      bot.send_message(CHAT_ID, message, parse_mode="HTML")
+      notified_matches.add(match_id)
 
 
-def fetch_participant_name(participant_id='233472101'):
-    """ get the team name """
-    url = f'https://api.challonge.com/v1/tournaments/{TOURNAMENT_URL}/participants/{participant_id}.json'
-    params = {'api_key': CHALLONGE_API_KEY}
-    headers = COOKIES
-    response = requests.get(url, 
-                            params=params,
-                            headers=headers)
-    participant = response.json()
-    return participant['participant']['name']
+def fetch_participant_name(participant_id="233472101"):
+  """get the team name"""
+  url = f"https://api.challonge.com/v1/tournaments/{TOURNAMENT_URL}/participants/{participant_id}.json"
+  params = {"api_key": CHALLONGE_API_KEY}
+  headers = COOKIES
+  response = requests.get(url, params=params, headers=headers)
+  participant = response.json()
+  return participant["participant"]["name"]
 
 
 def main():
-    while True:
-        notify_matches()
-        time.sleep(15)  # Fetch every 15 seconds
+  while True:
+    notify_matches()
+    time.sleep(15)  # Fetch every 15 seconds
 
 
-if __name__ == '__main__':
-    from threading import Thread
-    bot_thread = Thread(target=bot.polling, args=())
-    bot_thread.start()
-    main()
+if __name__ == "__main__":
+  from threading import Thread
+
+  bot_thread = Thread(target=bot.polling, args=())
+  bot_thread.start()
+  main()
