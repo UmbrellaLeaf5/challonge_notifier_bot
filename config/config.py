4905e45baa58e387cff00dd8dcbc957fbf6b6850
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2025-06-05 22:16:07 +0300

    add config.py

---
+++ config/config.py
@@ -0,0 +1,32 @@
+import json
+from typing import Any
+
+Config = dict[str, Any]
+
+# имя файла конфигурации.
+config_file_name: str = "config.json"
+
+# путь к файлу конфигурации.
+main_config_file_name: str = f"config/{config_file_name}"
+
+
+def GetConfig(file_name: str = main_config_file_name, encoding: str = "utf-8") -> Config:
+  """
+  Загружает конфигурацию из JSON-файла.
+
+  Args:
+      file_name (str): имя файла конфигурации.
+      encoding (str): кодировка файла.
+
+  Returns:
+      Config: словарь с параметрами конфигурации.
+  """
+
+  # открываем файл конфигурации.
+  with open(file_name, "r", encoding=encoding) as config:
+    # загружаем конфигурацию из JSON.
+    return json.load(config)
+
+
+# MEANS: словарь, содержащий параметры конфигурации.
+config = GetConfig()
